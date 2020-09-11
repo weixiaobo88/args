af29d817fac69e4ec6fb7b4c14796a1e3be8ba8e
Zhaoyu <weixiaobo88@gmail.com>
2020-09-11 10:51:28 +0800

    [Linne] - refactor: move argument parse logic to ArgumentParser

--- src/main/java/com/thoughtworks/basic/Args.java
+++ src/main/java/com/thoughtworks/basic/Args.java
@@ -1,61 +1,19 @@
 package com.thoughtworks.basic;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class Args {
-    private final Schema schema;
     private Lexer lexer;
+    private ArgumentParser argumentParser;
 
     public Args(Schema schema, Lexer lexer) {
-        this.schema = schema;
         this.lexer = lexer;
+        argumentParser = new ArgumentParser(schema);
     }
 
     List<Argument> analyze() {
-        List<Argument> arguments = new ArrayList<>();
-
         List<String> splitParts = lexer.parse();
 
-        splitParts.forEach(part -> {
-            ArgumentTO argumentTO = pickArgumentTO(part);
-            arguments.add(generateArgument(argumentTO));
-        });
-
-        return arguments;
-    }
-
-    private Argument generateArgument(ArgumentTO argumentTO) {
-        String flag = argumentTO.getFlag();
-        String value = argumentTO.getValue();
-        return new Argument(flag, parseValueBy(schema, flag, value));
-    }
-
-    private Object parseValueBy(Schema schema, String flag, String value) {
-        Object flagType = schema.getFlagType(flag);
-
-        if (flagType.equals(Boolean.class)) {
-            return Boolean.parseBoolean(value);
-        }
-
-        if (flagType.equals(Integer.class)) {
-            return Integer.parseInt(value);
-        }
-
-        return value;
-    }
-
-    private ArgumentTO pickArgumentTO(String item) {
-        String[] split = item.split("\\s+");
-        String flag = split[0].substring(1);
-
-        if (split.length > 1) {
-            String value = split[1];
-
-            return new ArgumentTO(flag, value);
-        }
-
-        String defaultValue = "";
-        return new ArgumentTO(flag, defaultValue);
+        return argumentParser.parse(splitParts);
     }
 }
---
+++ src/main/java/com/thoughtworks/basic/ArgumentParser.java
@@ -0,0 +1,57 @@
+package com.thoughtworks.basic;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class ArgumentParser {
+    private Schema schema;
+
+    ArgumentParser(Schema schema) {
+        this.schema = schema;
+    }
+
+    List<Argument> parse(List<String> splitParts) {
+        List<Argument> arguments = new ArrayList<>();
+
+        splitParts.forEach(part -> {
+            ArgumentTO argumentTO = pickArgumentTO(part);
+            arguments.add(generateArgument(argumentTO));
+        });
+
+        return arguments;
+    }
+
+    private ArgumentTO pickArgumentTO(String item) {
+        String[] split = item.split("\\s+");
+        String flag = split[0].substring(1);
+
+        if (split.length > 1) {
+            String value = split[1];
+
+            return new ArgumentTO(flag, value);
+        }
+
+        String defaultValue = "";
+        return new ArgumentTO(flag, defaultValue);
+    }
+
+    private Argument generateArgument(ArgumentTO argumentTO) {
+        String flag = argumentTO.getFlag();
+        String value = argumentTO.getValue();
+        return new Argument(flag, parseValueBy(schema, flag, value));
+    }
+
+    private Object parseValueBy(Schema schema, String flag, String value) {
+        Object flagType = schema.getFlagType(flag);
+
+        if (flagType.equals(Boolean.class)) {
+            return Boolean.parseBoolean(value);
+        }
+
+        if (flagType.equals(Integer.class)) {
+            return Integer.parseInt(value);
+        }
+
+        return value;
+    }
+}
